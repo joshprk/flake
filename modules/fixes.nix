a4ae7357b3281fdd4fc9ce63d702f8e6d2033edc
Joshua Park <joshuprk@gmail.com>
2025-03-07 15:27:55 -0500

    refactor: separate webhid and mouse wakeup fix

--- modules/fixes.nix
+++ modules/fixes.nix
@@ -8,15 +8,19 @@ in {
   options.settings = {
     fixes = {
       mouseWakeup = lib.mkEnableOption "the mouse wakeup fix";
+      webhid = lib.mkEnableOption "the WebHID fix";
     };
   };
 
   config = lib.mkMerge [
     (lib.mkIf cfg.mouseWakeup {
       services.udev.extraRules = ''
+        ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c547", ATTR{power/wakeup}="disabled"
+      '';
+    })
+    (lib.mkIf cfg.webhid {
+      services.udev.extraRules = ''
         KERNEL=="hidraw*", SUBSYSTEM=="hidraw", GROUP="wheel", MODE="0660"
-        ACTION=="add", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c094", ATTR{power/wakeup}="disabled"
-        ACTION=="add", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c547", ATTR{power/wakeup}="disabled"
       '';
     })
   ];
