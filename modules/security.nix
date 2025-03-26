c2161d26ba8101bc561af62050f9afbe0afc4e32
Joshua Park <joshuprk@gmail.com>
2025-03-25 23:49:16 -0400

    fix: fix typo

--- modules/security.nix
+++ modules/security.nix
@@ -11,7 +11,7 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.secureBoot {
+  config = lib.mkIf cfg.secureboot {
     boot = {
       initrd.systemd.enable = lib.mkForce true;
       bootspec.enable = lib.mkForce true;
