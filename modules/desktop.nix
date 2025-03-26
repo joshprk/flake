cbc5097df8383125cda8f64af2b6b5379bfd6e7a
Joshua Park <joshuprk@gmail.com>
2025-03-25 20:49:21 -0400

    feat: implement hyprland

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.settings.desktop;
@@ -8,11 +9,52 @@ in {
   options.settings = {
     desktop = {
       enable = lib.mkEnableOption "the desktop module";
+      hyprland = lib.mkEnableOption "the Hyprland window manager";
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    settings.networking.enable = true;
-    powerManagement.enable = true;
-  };
+  config = let
+    general = {
+      settings.networking.enable = true;
+      powerManagement.enable = true;
+    };
+
+    hyprland = {
+      environment.pathsToLink = [
+        "/share/xdg-desktop-portal"
+        "/share/applications"
+      ];
+
+      programs.hyprland = {
+        enable = true;
+      };
+
+      services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+      };
+
+      services.xserver = {
+        enable = true;
+        desktopManager.xterm.enable = false;
+        displayManager.lightdm.enable = false;
+      };
+
+      xdg = {
+        autostart.enable = true;
+        portal = {
+          enable = true;
+          extraPortals = with pkgs; [
+            xdg-desktop-portal-hyprland
+            xdg-desktop-portal-gtk
+          ];
+        };
+      };
+
+      security.rtkit.enable = true;
+    };
+  in lib.mkIf cfg.enable (lib.mkMerge [
+    general
+    (lib.mkIf cfg.hyprland hyprland)
+  ]);
 }
