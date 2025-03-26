ae1244182ed1e18590748415b0656d94cb7ca34b
Joshua Park <joshuprk@gmail.com>
2025-03-26 08:19:19 -0400

    feat: enable systemd integration for waybar

--- users/joshua/waybar.nix
+++ users/joshua/waybar.nix
@@ -14,6 +14,7 @@ in {
   config = lib.mkIf cfg.enable {
     programs.waybar = {
       enable = true;
+      systemd.enable = true;
       settings = {
         mainBar = {
           layer = "top";
