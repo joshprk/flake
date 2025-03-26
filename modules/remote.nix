1a691049c731f55ea311bf7c7c5317d1a2aa6cd2
Joshua Park <joshuprk@gmail.com>
2025-03-25 23:57:49 -0400

    feat: add remote module

---
+++ modules/remote.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.settings.remote;
+in {
+  options.settings = {
+    remote = {
+      server = {
+        enable = lib.mkEnableOption "the remote server module";
+      };
+
+      client = {
+        enable = lib.mkEnableOption "the remote client module";
+      };
+    };
+  };
+
+  config = let
+    server = {
+      services.openssh = {
+        enable = true;
+        startWhenNeeded = true;
+      };
+
+      settings.impermanence.extraDirectories = [
+        "/etc/ssh"
+      ];
+    };
+
+    client = {
+      services.tailscale = {
+        enable = true;
+        useRoutingFeatures = "server";
+      };
+
+      settings.impermanence.extraDirectories = [
+        "/var/lib/tailscale"
+      ];
+    };
+  in lib.mkMerge [
+    (lib.mkIf cfg.server.enable server)
+    (lib.mkIf cfg.client.enable client)
+  ];
+}
