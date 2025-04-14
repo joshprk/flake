c04c633a8fe836fe780e77575a32986c8b53a13b
Joshua Park <joshuprk@gmail.com>
2025-04-13 21:43:11 -0400

    feat: implement temporary home-manager config for joshua

--- users/joshua/default.nix
+++ users/joshua/default.nix
@@ -1,4 +1,22 @@
-{...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.username = "joshua";
   home.stateVersion = "25.05";
+
+  /* temporary configuration until modules */
+  programs.ghostty = {
+    enable = true;
+  };
+
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri-unstable;
+    settings.binds = {
+      "Mod+Q".action.spawn = "ghostty";
+    };
+  };
 }
