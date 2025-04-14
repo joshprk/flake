137fc9b566a9dd67a815a6b1a30b7b5fbebf2419
Joshua Park <joshuprk@gmail.com>
2025-04-13 20:21:57 -0400

    feat: add hardware configurations

--- hosts/PC/default.nix
+++ hosts/PC/default.nix
@@ -3,6 +3,10 @@
   lib,
   ...
 }: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
   networking.hostName = "PC";
   system.stateVersion = "25.05";
   nixpkgs.hostPlatform = "x86_64-linux";
