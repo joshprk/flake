5a04287fbdab243b4136fbaf564011b94532c64d
Joshua Park <joshuprk@gmail.com>
2025-04-14 19:19:35 -0400

    feat: set tab length to 2 spaces

--- home/nvim.nix
+++ home/nvim.nix
@@ -14,6 +14,13 @@ in {
   config = lib.mkIf cfg.enable {
     programs.nixvim = {
       enable = true;
+      defaultEditor = true;
+      opts = {
+        expandtab = true;
+        tabstop = 2;
+        softtabstop = 2;
+        shiftwidth = 2;
+      };
     };
   };
 }
