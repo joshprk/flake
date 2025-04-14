f83af53979a43fe925f0c12cd64b9f06e66edfdc
Joshua Park <joshuprk@gmail.com>
2025-04-14 00:14:17 -0400

    feat: implement experimental firefox

--- users/joshua/default.nix
+++ users/joshua/default.nix
@@ -8,6 +8,39 @@
   home.stateVersion = "25.05";
 
   /* temporary configuration until modules */
+  programs.firefox = {
+    enable = true;
+    languagePacks = ["en-US"];
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      ExtensionSettings = {
+        "*".installation_mode = "blocked";
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+  };
+
   programs.ghostty = {
     enable = true;
   };
