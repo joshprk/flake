6ce7c8ca7682325576258103fe77eb748f7ab02c
Joshua Park <joshuprk@gmail.com>
2025-04-14 20:49:58 -0400

    feat: disable sponsored firefox things

--- users/joshua/default.nix
+++ users/joshua/default.nix
@@ -24,12 +24,14 @@
     policies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
+
       EnableTrackingProtection = {
         Value = true;
         Locked = true;
         Cryptomining = true;
         Fingerprinting = true;
       };
+
       DisablePocket = true;
       DisableFirefoxAccounts = true;
       DisableAccounts = true;
@@ -37,6 +39,7 @@
       OverrideFirstRunPage = "";
       OverridePostUpdatePage = "";
       DontCheckDefaultBrowser = true;
+
       ExtensionSettings = {
         "*".installation_mode = "blocked";
         "uBlock0@raymondhill.net" = {
@@ -47,6 +50,37 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           installation_mode = "force_installed";
         };
+      };
+
+      Preferences = let
+        lock-false = {
+          Value = false;
+          Status = "locked";
+        };
+
+        lock-true = {
+          Value = true;
+          Status = "locked";
+        };
+      in {
+        "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+        "extensions.pocket.enabled" = lock-false;
+        "extensions.screenshots.disabled" = lock-true;
+        "browser.topsites.contile.enabled" = lock-false;
+        "browser.formfill.enable" = lock-false;
+        "browser.search.suggest.enabled" = lock-false;
+        "browser.search.suggest.enabled.private" = lock-false;
+        "browser.urlbar.suggest.searches" = lock-false;
+        "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
       };
     };
   };
