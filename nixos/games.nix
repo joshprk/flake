22cb138222c0942297a9ef8382d42185739c2522
Joshua Park <joshuprk@gmail.com>
2025-04-15 11:18:10 -0400

    fix: attempt to fix experimental games sandbox

--- nixos/games.nix
+++ nixos/games.nix
@@ -6,23 +6,24 @@
 }: let
   cfg = config.settings.games;
 
-  sandbox = pkgs.buildFHSEnv {
-    name = "games";
+  mkSandbox = lib.mkOverridable (package:
+    pkgs.buildFHSEnv {
+      inherit (package) name passthru;
 
-    targetPkgs = pkgs: with pkgs; [
-      steam-unwrapped
-    ];
+      targetPkgs = pkgs: with pkgs; [package];
+      mainProgram = lib.getExe package;
+      runScript = package.name;
 
-    chdirToPwd = false;
-    dieWithParent = true;
+      chdirToPwd = false;
+      dieWithParent = true;
 
-    extraBwrapArgs = [
-      "--chdir $HOME/.local/share/games"
-      "--bind $XDG_DATA_HOME/games $HOME"
-      "--dir $HOME/.local/share/games"
-      "--dir $HOME"
-    ];
-  };
+      extraBwrapArgs = [
+        "--chdir $HOME/.local/share/games"
+        "--bind $XDG_DATA_HOME/games $HOME"
+        "--dir $HOME/.local/share/games"
+        "--dir $HOME"
+      ];
+    });
 in {
   options.settings = {
     games = {
@@ -31,14 +32,9 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [sandbox];
-
     programs.steam = {
       enable = true;
-      package =
-        lib.makeOverridable
-        (args: pkgs.runCommand "steam" {nativeBuildInputs = [sandbox];} "steam")
-        {};
+      package = mkSandbox pkgs.steam;
     };
   };
 }
