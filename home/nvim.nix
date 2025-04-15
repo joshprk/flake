6b889d0bbc9726bcf4b500f4480306aee37c3e2b
Joshua Park <joshuprk@gmail.com>
2025-04-15 18:26:53 -0400

    feat: implement new nvim options

--- home/nvim.nix
+++ home/nvim.nix
@@ -15,12 +15,48 @@ in {
     programs.nixvim = {
       enable = true;
       defaultEditor = true;
+
       opts = {
         expandtab = true;
         tabstop = 2;
         softtabstop = 2;
         shiftwidth = 2;
+
+        cursorline = true;
+        number = true;
+        relativenumber = true;
+        wrap = false;
+
+        hidden = false;
+        splitright = true;
+        splitbelow = true;
+        
+        laststatus = 3;
       };
+
+      globals = {
+        loaded_netrwPlugin = 1;
+        loaded_netrwSettings = 1;
+        loaded_netrwFileHandlers = true;
+        loaded_netrw_gitignore = true;
+        loaded_netrw = 1;
+      };
+
+      highlight = {
+        DiagnosticUnderlineError.undercurl = true;
+        DiagnosticUnderlineHint.undercurl = true;
+        DiagnosticUnderlineInfo.undercurl = true;
+        DiagnosticUnderlineOk.undercurl = true;
+        DiagnosticUnderlineWarn.undercurl = true;
+      };
+
+      keymaps = [
+        {
+          action = ''"+ygv'';
+          key = "<C-c>";
+          mode = ["v"];
+        }
+      ];
     };
   };
 }
