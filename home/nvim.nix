5d901d3cdc8e0f435571977f23be5c2b407d0064
Joshua Park <joshuprk@gmail.com>
2025-04-16 02:05:05 -0400

    feat: add options to nvim

--- home/nvim.nix
+++ home/nvim.nix
@@ -7,7 +7,7 @@
 in {
   options.settings = {
     nvim = {
-      enable = lib.mkEnableOption "the Nvim home module";
+      enable = lib.mkEnableOption "the Neovim home module";
     };
   };
 
@@ -30,11 +30,18 @@ in {
         hidden = false;
         splitright = true;
         splitbelow = true;
-        
+
+        swapfile = false;
+        undofile = true;
+
         laststatus = 3;
       };
 
       globals = {
+        loaded_ruby_provider = 0;
+        loaded_perl_provider = 0;
+        loaded_python_provider = 0;
+
         loaded_netrwPlugin = 1;
         loaded_netrwSettings = 1;
         loaded_netrwFileHandlers = true;
@@ -57,6 +64,58 @@ in {
           mode = ["v"];
         }
       ];
+
+      plugins.blink-cmp = {
+        enable = true;
+
+        lazyLoad.settings.event = [
+          "InsertEnter"
+          "CmdlineEnter"
+        ];
+      };
+
+      plugins.bufferline = {
+        enable = true;
+
+        lazy.settings.event = [
+          "DeferredUIEnter"
+        ];
+
+        settings = {
+          options = {
+            mode = "buffers";
+            always_show_bufferline = true;
+          };
+        };
+      };
+
+      plugins.lualine = {
+        enable = true;
+
+        lazyLoad.settings.event = [
+          "VimEnter"
+          "BufReadPost"
+          "BufNewFile"
+        ];
+      };
+
+      plugins.noice = {
+        enable = true;
+
+        lazyLoad.settings.event = [
+          "DeferredUIEnter"
+        ];
+
+        settings.presets = {
+          bottom_search = false;
+          command_palette = true;
+        };
+      };
+
+      performance = {
+        enable = true;
+        nvimRuntime = true;
+      };
     };
   };
 }
