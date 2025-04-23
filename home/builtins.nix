ac2e6d7471a4b6b5b8d03f2264acd66c978e85a7
Joshua Park <joshuprk@gmail.com>
2025-04-23 02:16:56 -0400

    feat: respect xdg base directories for cargo and npm

--- home/builtins.nix
+++ home/builtins.nix
@@ -5,7 +5,18 @@
 }: {
   config = {
     home.sessionVariables = {
+      CARGO_HOME = "${config.xdg.dataHome}/cargo";
+      NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
       XCOMPOSECACHE = "${config.xdg.cacheHome}/X11/xcompose";
+    };
+
+    home.file = {
+      "${config.xdg.configHome}/npm/npmrc".text = ''
+        prefix=${config.xdg.dataHome}/npm
+        cache=${config.xdg.cacheHome}/npm
+        init-module=${config.xdg.configHome}/npm/config/npm-init.js
+        logs-dir=${config.xdg.stateHome}/npm/logs
+      '';
     };
 
     xdg.enable = true;
