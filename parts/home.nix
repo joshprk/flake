f82d01d40d7400a684640b7320b52aa304267dd7
Joshua Park <joshuprk@gmail.com>
2025-05-18 11:58:50 +0000

    feat: set default shell to fish

--- parts/home.nix
+++ parts/home.nix
@@ -6,14 +6,17 @@
    * to be stabilized than finding someone who'd want to use my systems.
    */
   flake = {
-    nixosModules.default.imports = lib.singleton ({...}: {
+    nixosModules.default.imports = lib.singleton ({pkgs, ...}: {
       users.users.joshua = {
         isNormalUser = true;
 	extraGroups = ["wheel"];
+	shell = pkgs.fish;
 	openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYMjdyYJfwjRqHuyePy0xNRKYxeSuJ6e9I1g9F0eHsD"
 	];
       };
+
+      programs.fish.enable = true;
 
       home-manager.users.joshua = {
         home.username = "joshua";
