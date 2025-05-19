d0f3a7e4d358dbceefdbaf6e6aab654828581a31
Joshua Park <joshuprk@gmail.com>
2025-05-19 15:46:03 +0000

    refactor: unscope for nicer styling

--- nixos/kernel.nix
+++ nixos/kernel.nix
@@ -1,8 +1,6 @@
 {lib, ...}: {
   config = {
-    boot.loader = {
-      systemd-boot.enable = lib.mkDefault true;
-      efi.canTouchEfiVariables = lib.mkDefault true;
-    };
+    boot.loader.systemd-boot.enable = lib.mkDefault true;
+    boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
   };
 }
