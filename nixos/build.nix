53a0c829f16c276bbd38a32140378b5b16b2d63c
Joshua Park <joshuprk@gmail.com>
2025-05-19 15:45:20 +0000

    refactor: move build to options

--- nixos/build.nix
+++ nixos/build.nix
@@ -1,20 +1,25 @@
-{pkgs, ...}: {
+{config, lib, pkgs, ...}: let
+  cfg = config.modules.build;
+in {
+  options.modules.build = {
+    flakePath = {
+      type = lib.types.str;
+      description = "Defines where the updated flake should be fetched from.";
+      default = "github:joshprk/flake";
+    };
+  };
+
   config = {
     environment.shellAliases = {
       update = "${pkgs.writeShellScript "update-command" ''
-        FLAKE="github:joshprk/flake"
+        FLAKE="${config.modules.build.flakePath}"
         LAST_DRV="$(readlink /nix/var/nix/profiles/system --canonicalize)"
-
         nixos-rebuild switch --refresh --use-remote-sudo --flake $FLAKE
-
         NEW_DRV="$(readlink /nix/var/nix/profiles/system --canonicalize)"
-
         if [[ "$LAST_DRV" != "$NEW_DRV" ]]; then
           DIFF=$(nix store diff-closures "$LAST_DRV" "$NEW_DRV")
-
           if [[ "$DIFF" != "" ]]; then
-            echo "---"
-            echo "$DIFF"
+            echo "---\n$DIFF"
           fi
         fi
       ''}";
