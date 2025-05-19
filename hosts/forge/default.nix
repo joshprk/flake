112474dbcd688be71928952de60b156f5642c90a
Joshua Park <joshuprk@gmail.com>
2025-05-19 16:31:48 +0000

    feat: enable impermanence module

--- hosts/forge/default.nix
+++ hosts/forge/default.nix
@@ -7,6 +7,10 @@
   networking.hostName = "forge";
   system.stateVersion = "25.11";
 
+  modules.impermanence = {
+    enable = true;
+  };
+
   users.users.root = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOimZhc+sD7K1zHQgAX66KpB2L/daf6fxIix541Sb7I"
