3bea8b16195e84dc598ce26c7a9405f91f6c2a78
Joshua Park <joshuprk@gmail.com>
2025-05-19 15:45:07 +0000

    refactor: remove unnecessary variables

--- nixos/env.nix
+++ nixos/env.nix
@@ -1,9 +1,5 @@
 {...}: {
   config = {
     environment.enableAllTerminfo = true;
-    environment.variables = {
-      TERMINFO = "$XDG_DATA_HOME/terminfo";
-      TERMINFO_DIRS = "$XDG_DATA_HOME/terminfo:/usr/share/terminfo";
-    };
   };
 }
