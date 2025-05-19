cab9a53bbfc2339447c3418c060f0fa5807fc8e2
Joshua Park <joshuprk@gmail.com>
2025-05-19 06:02:33 +0000

    feat: add update command

--- nixos/build.nix
+++ nixos/build.nix
@@ -1,6 +1,27 @@
-{...}: {
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    use-xdg-base-directories = true;
+{pkgs, ...}: {
+  config = {
+    environment.shellAliases = {
+      update = "${pkgs.writeShellScript ''
+        LAST_DRV="$(readlink /nix/var/nix/profiles/system --canonicalize)"
+
+        nixos-rebuild switch --refresh --use-remote-sudo --flake $FLAKE
+
+        NEW_DRV="$(readlink /nix/var/nix/profiles/system --canonicalize)"
+
+        if [[ "$LAST_DRV" != "$NEW_DRV" ]]; then
+          DIFF=$(nix store diff-closures "$LAST_DRV" "$NEW_DRV")
+
+          if [[ "$DIFF" != "" ]]; then
+            echo "---"
+            echo "$DIFF"
+          fi
+        fi
+      ''}";
+    };
+
+    nix.settings = {
+      experimental-features = ["nix-command" "flakes"];
+      use-xdg-base-directories = true;
+    };
   };
 }
