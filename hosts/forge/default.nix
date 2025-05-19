8186c0ef3c7382208b29cabdf87925fc88cff12d
Joshua Park <joshuprk@gmail.com>
2025-05-19 15:30:41 +0000

    refactor: move forge disko to separate file

--- hosts/forge/default.nix
+++ hosts/forge/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./disko.nix
     ./hardware-configuration.nix
   ];
 
@@ -13,59 +14,5 @@
   };
 
   services.openssh.enable = true;
-
-  disko.devices.disk.system = {
-    device = "/dev/sda";
-    type = "disk";
-    content.type = "gpt";
-    content.partitions = {
-      boot = {
-        name = "boot";
-        size = "512M";
-        type = "EF00";
-        content = {
-          type = "filesystem";
-          format = "vfat";
-          mountpoint = "/boot";
-          mountOptions = ["umask=0077"];
-        };
-      };
-
-      nix = {
-        name = "nix";
-        size = "100%";
-        content = {
-          type = "filesystem";
-          format = "btrfs";
-          mountpoint = "/nix";
-          mountOptions = ["compress=zstd" "noatime"];
-        };
-      };
-    };
-  };
-
-  disko.devices.disk.home = {
-    device = "/dev/sdb";
-    type = "disk";
-    content.type = "gpt";
-    content.partitions = {
-      home = {
-        name = "home";
-        size = "100%";
-        content = {
-          type = "filesystem";
-          format = "btrfs";
-          mountpoint = "/home";
-          mountOptions = ["compress=zstd" "noatime"];
-        };
-      };
-    };
-  };
-
-  disko.devices.nodev."/" = {
-    fsType = "tmpfs";
-    mountOptions = ["size=8G" "defaults" "mode=755"];
-  };
-
   zramSwap.enable = true;
 }
---
+++ hosts/forge/disko.nix
@@ -0,0 +1,51 @@
+{...}: {
+  disko.devices.disk.system = {
+    device = "/dev/sda";
+    type = "disk";
+    content.type = "gpt";
+
+    content.partitions.boot = {
+      name = "boot";
+      size = "512M";
+      type = "EF00";
+      content = {
+        type = "filesystem";
+	format = "vfat";
+	mountpoint = "/boot";
+	mountOptions = ["umask=0077"];
+      };
+
+    content.partitions.nix = {
+      name = "nix";
+      size = "100%";
+      content = {
+        type = "filesystem";
+	format = "btrfs";
+	mountpoint = "/nix";
+	mountOptions = ["compress=zstd" "noatime"];
+      };
+    };
+  };
+
+  disko.devices.disk.home = {
+    device = "/dev/sdb";
+    type = "disk";
+    content.type = "gpt";
+
+    content.partitions.home = {
+      name = "home";
+      size = "100%";
+      content = {
+	type = "filesystem";
+	format = "btrfs";
+        mountpoint = "/home";
+        mountOptions = ["compress=zstd" "noatime"];
+      };
+    };
+  };
+
+  disko.devices.nodev."/" = {
+    fsType = "tmpfs";
+    mountOptions = ["size=8G" "defaults" "mode=755"];
+  };
+}
