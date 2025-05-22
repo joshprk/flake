fb75b10ead5f75c287ff07cdb2b9e52835e001ae
Joshua Park <123624726+joshprk@users.noreply.github.com>
2025-05-22 19:11:27 +0000

    fix: respect xdg base directory for histfile

--- nixos/env.nix
+++ nixos/env.nix
@@ -4,10 +4,15 @@
 
   config = {
     environment = {
-      enableAllTerminfo = true;
       variables = {
         NIXOS_OZONE_WL = "1";
       };
+      enableAllTerminfo = true;
     };
+
+    programs.bash.interactiveShellInit = ''
+      [ -d "$XDG_STATE_HOME"/bash ] || mkdir -p "$XDG_STATE_HOME"/bash
+      export HISTFILE="$XDG_STATE_HOME"/bash/history
+    '';
   };
 }
