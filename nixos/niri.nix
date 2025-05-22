0dc59a272aa7eedb6c3989632f9cbd8c0a1b0398
Joshua Park <123624726+joshprk@users.noreply.github.com>
2025-05-22 18:52:26 +0000

    feat: implement niri module

---
+++ nixos/niri.nix
@@ -0,0 +1,53 @@
+{config, lib, pkgs, ...}: let
+  cfg = config.modules.niri;
+in {
+  options.modules.niri = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Whether to enable the niri module.";
+      default = false;
+    };
+
+    gnome-keyring = lib.mkOption {
+      type = lib.types.bool;
+      description = "Whether to enable the Gnome Keyring";
+      default = false;
+    };
+
+    polkit.enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Whether to enable polkit in the niri module.";
+      default = false;
+    };
+
+    polkit.command = lib.mkOption {
+      type = lib.types.str;
+      description = "The command to use to start polkit.";
+      default = "${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Sensible and minimal defaults for sodiboo/niri-flake
+    modules.home.interactive = lib.mkDefault true;
+    programs.niri.enable = true;
+
+    services.gnome.gnome-keyring.enable = lib.mkForce cfg.gnome-keyring;
+    security.polkit.enable = lib.mkForce cfg.polkit.enable;
+
+    systemd.user.services.niri-flake-polkit = {
+      enable = lib.mkForce cfg.polkit.enable;
+      serviceConfig.ExecStart = lib.mkForce cfg.polkit.command;
+    };
+
+    # Audio setup
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+
+    # Enable networking by default to prevent lockout
+    networking.networkmanager.enable = lib.mkDefault true;
+  };
+}
