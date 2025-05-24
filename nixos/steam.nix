f478792c717caa94027777074bc9cac122a694c8
Joshua Park <123624726+joshprk@users.noreply.github.com>
2025-05-24 18:28:45 +0000

    feat: implement steam module

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -12,12 +12,29 @@ in {
       description = "Whether to enable the Steam module.";
       default = false;
     };
+
+    directory = lib.mkOption {
+      type = lib.types.str;
+      description = "Which path to contain Steam files in.";
+      default = "$XDG_DATA_HOME/steam";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     programs.steam = {
       enable = true;
-      package = pkgs.steam; # need to sandbox
+      package = let
+        overlay = prev: pkgs.buildFHSEnv (prev // {
+          extraBwrapArgs = prev.extraBwrapArgs ++ [
+            "--bind ${cfg.directory} $HOME"
+            "--chdir ${cfg.directory}"
+            "--dir ${cfg.directory}"
+          ];
+          chdirToPwd = false;
+          dieWithParent = true;
+        });
+      in
+        pkgs.steam.override {buildFHSEnv = overlay;};
       extraCompatPackages = with pkgs; [proton-ge-bin];
     };
   };
