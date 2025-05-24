77568ef0f2910cdc8ada732245ede94f5532f010
Joshua Park <123624726+joshprk@users.noreply.github.com>
2025-05-24 19:16:47 +0000

    feat: add manual tailscale

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -11,9 +11,23 @@ in {
       description = "Whether to enable the networking module.";
       default = false;
     };
+
+    addToTailnet = lib.mkOption {
+      type = lib.types.bool;
+      description = "Whether to add this host to the Tailnet.";
+      default = true;
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    modules.impermanence.extraDirectories = lib.mkIf cfg.addToTailnet [
+      "/var/lib/tailscale"
+    ];
+
+    services.tailscale = lib.mkIf cfg.addToTailnet {
+      enable = true;
+    };
+
     services.resolved = {
       enable = true;
     };
