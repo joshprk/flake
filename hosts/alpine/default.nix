1b1ba0d612e8ebf1d065ef85346ac28b31753f39
Joshua Park <123624726+joshprk@users.noreply.github.com>
2025-05-25 20:46:20 -0400

    feat: alpine is online

--- hosts/alpine/default.nix
+++ hosts/alpine/default.nix
@@ -1,9 +1,33 @@
 {...}: {
-  imports = [];
+  imports = [
+    ./disko.nix
+    ./hardware-configuration.nix
+  ];
 
-  nixpkgs.hostPlatform = "x86_64-linux";
   networking.hostName = "alpine";
   system.stateVersion = "25.11";
+
+  modules = {
+    home.enable = true;
+    home.interactive = true;
+    impermanence.enable = true;
+    niri.enable = true;
+    nvidia = {
+      enable = true;
+      tuning = {
+        enable = true;
+        gpuClock = 1700;
+        memoryClock = 10001;
+      };
+      prime = {
+        offload.enable = true;
+        nvidiaBusId = "PCI:1:0:0";
+        amdgpuBusId = "PCI:13:0:0";
+      };
+    };
+    openssh.enable = true;
+    steam.enable = true;
+  };
 
   zramSwap.enable = true;
 }
---
+++ hosts/alpine/disko.nix
@@ -0,0 +1,54 @@
+{...}: {
+  disko.devices.disk.system = {
+    device = "/dev/nvme0n1";
+    type = "disk";
+    content.type = "gpt";
+    content.partitions.boot = {
+      name = "boot";
+      size = "512M";
+      type = "EF00";
+      content = {
+        type = "filesystem";
+        format = "vfat";
+        mountpoint = "/boot";
+        mountOptions = ["umask=0077"];
+      };
+    };
+
+    content.partitions.nix = {
+      size = "100%";
+      content = {
+        name = "nix";
+        type = "luks";
+        settings.allowDiscards = true;
+        content.type = "filesystem";
+        content.format = "btrfs";
+        content.mountpoint = "/nix";
+        content.mountOptions = ["compress=zstd" "noatime"];
+      };
+    };
+  };
+
+  disko.devices.disk.data = {
+    device = "/dev/nvme1n1";
+    type = "disk";
+    content.type = "gpt";
+    content.partitions.data = {
+      size = "100%";
+      content = {
+        name = "data";
+        type = "luks";
+        settings.allowDiscards = true;
+        content.type = "filesystem";
+        content.format = "btrfs";
+        content.mountpoint = "/home";
+        content.mountOptions = ["compress=zstd" "noatime"];
+      };
+    };
+  };
+
+  disko.devices.nodev."/" = {
+    fsType = "tmpfs";
+    mountOptions = ["size=50%" "defaults" "mode=755"];
+  };
+}
