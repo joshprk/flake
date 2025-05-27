696df3690c605d8dcc1c26cd4ef9e9256bbe3927
Joshua Park <123624726+joshprk@users.noreply.github.com>
2025-05-27 12:50:02 -0400

    chore: table this project for another time

--- hosts/forge/default.nix
+++ hosts/forge/default.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{...}: {
   imports = [
     ./disko.nix
     ./hardware-configuration.nix
@@ -16,38 +11,6 @@
     home.enable = true;
     openssh.enable = true;
     impermanence.enable = true;
-  };
-
-  services.caddy = {
-    enable = true;
-    virtualHosts."forge.joshprk.me".extraConfig = ''
-      reverse_proxy ${config.containers.vaultwarden.hostAddress}:8222
-    '';
-  };
-
-  containers.vaultwarden = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "192.168.100.1";
-    config = {...}: {
-      services.vaultwarden = {
-        enable = true;
-        config = {
-          ROCKET_ADDRESS = "0.0.0.0";
-          ROCKET_PORT = 8222;
-        };
-      };
-      nixpkgs.pkgs = pkgs;
-      system.stateVersion = "25.11";
-    };
-  };
-
-  networking.firewall.interfaces."tailscale0" = {
-    allowedTCPPorts = [80 443];
-  };
-
-  users.users.root = {
-    openssh.authorizedKeys.keys = [config.modules.secrets.pubkeyStore.root];
   };
 
   zramSwap.enable = true;
