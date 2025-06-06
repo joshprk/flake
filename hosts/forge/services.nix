2f7102dc9b84ab028853c221a7c4a39f2bbe36e3
Joshua Park <123624726+joshprk@users.noreply.github.com>
2025-06-06 02:20:52 -0400

    feat: test tailscale https

--- hosts/forge/services.nix
+++ hosts/forge/services.nix
@@ -1,4 +1,6 @@
-{pkgs, ...}: {
+{config, lib, pkgs, ...}: let
+  tsConfig = config.services.tailscale;
+in {
   containers.private = {
     autoStart = true;
     privateNetwork = true;
@@ -6,7 +8,6 @@
       services.vaultwarden = {
         enable = true;
       };
-
       nixpkgs = {inherit pkgs;};
     };
   };
@@ -14,21 +15,21 @@
   services.caddy = {
     enable = true;
     virtualHosts."forge.joshprk.me".extraConfig = ''
-      @tailscale {
-        remote_ip 100.64.0.0/10
-      }
-      
-      handle @tailscale {
-        respond "You are authenticated!"
-      }
-
-      handle {
-        respond "Access denied" 403
-      }
+      respond "Hi tailnet!"
     '';
   };
 
-  networking.firewall = {
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      address = "/forge.joshprk.me/100.107.152.76";
+    };
+  };
+
+  services.tailscale.permitCertUid = "caddy";
+
+  networking.firewall.interfaces.${tsConfig.interfaceName} = {
     allowedTCPPorts = [80 443];
+    allowedUDPPorts = [53];
   };
 }
