d184264e42ae81c4a769f9d549a34a6c19c4fa74
Joshua Park <123624726+joshprk@users.noreply.github.com>
2025-06-07 14:36:33 -0400

    fix: ensure internal routes block unauthorized subnets

--- hosts/forge/services.nix
+++ hosts/forge/services.nix
@@ -6,6 +6,7 @@
 }: let
   tsConfig = config.services.tailscale;
 in {
+  # Internal services which are accessible only through private routes
   containers.private = {
     autoStart = true;
     config = {
@@ -27,24 +28,40 @@ in {
       plugins = ["github.com/caddy-dns/porkbun@v0.3.1"];
       hash = "sha256-7TqepCX9F5AMAUJrH8wxdnrr3JMezhowyIPlfFYUQG8=";
     };
+
     globalConfig = ''
       acme_dns porkbun {
         api_key {env.PORKBUN_API_KEY}
         api_secret_key {env.PORKBUN_SEC_KEY}
       }
     '';
-    virtualHosts."forge.joshprk.me".extraConfig = ''
-      respond "Hi tailnet!"
-    '';
-    virtualHosts."vault.joshprk.me".extraConfig = ''
-      reverse_proxy 127.0.0.1:8222
-    '';
+
+    # Ensure private routes block all external requests
+    virtualHosts =
+      builtins.mapAttrs (
+        _: v: v // {
+          extraConfig = ''
+            @denied not remote_ip private_ranges 100.64.0.0/10
+            abort @denied
+            ${v.extraConfig}
+          '';
+        }
+      ) {
+        "forge.joshprk.me".extraConfig = ''
+          respond "Hi tailnet!"
+        '';
+        "vault.joshprk.me".extraConfig = ''
+          reverse_proxy 127.0.0.1:8222
+        '';
+      };
+
     environmentFile = config.age.secrets.caddy-env.path;
   };
 
   services.dnsmasq = {
     enable = true;
     resolveLocalQueries = false;
+
     settings = {
       address = [
         "/forge.joshprk.me/100.87.235.15"
