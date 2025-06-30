973c8a02e4f1cd54772ea812d57b43b501b27102
Joshua Park <git@joshprk.me>
2025-06-29 20:44:34 -0400

    refactor: cleanup nvidia module

--- nixos/system/nvidia.nix
+++ nixos/system/nvidia.nix
@@ -8,15 +8,6 @@
   cfg = config.modules.system.nvidia;
   tuneMem = cfg.maxMemClock != null;
   tuneGpu = cfg.maxGpuClock != null;
-  smi = "/run/current-system/sw/bin/nvidia-smi";
-  tuneMemCmd =
-    if tuneMem
-    then "${smi} --lock-memory-clocks=0,${builtins.toString cfg.maxMemClock}"
-    else "";
-  tuneGpuCmd =
-    if tuneGpu
-    then "${smi} --lock-gpu-clocks=0,${builtins.toString cfg.maxGpuClock}"
-    else "";
 in {
   options.modules.system.nvidia = {
     enable = lib.mkEnableOption "the nvidia module";
@@ -24,24 +15,26 @@ in {
     package = lib.mkOption {
       type = lib.types.package;
       default = config.boot.kernelPackages.nvidiaPackages.beta;
-      description = ''
-        Which nvidia driver package to use.
-      '';
+      description = "Which nvidia driver package to use.";
     };
 
     maxGpuClock = lib.mkOption {
       type = with lib.types; nullOr ints.positive;
-      description = ''
-        Maximum GPU clock in megahertz.
-      '';
+      description = "Maximum GPU clock in megahertz.";
+      apply = opt:
+        if opt != null
+        then builtins.toString opt
+        else opt;
       default = null;
     };
 
     maxMemClock = lib.mkOption {
       type = with lib.types; nullOr ints.positive;
-      description = ''
-        Maximum memory clock in megahertz.
-      '';
+      description = "Maximum memory clock in megahertz.";
+      apply = opt:
+        if opt != null
+        then builtins.toString opt
+        else opt;
       default = null;
     };
 
@@ -67,12 +60,24 @@ in {
     };
 
     systemd.services.nvidia-tuning = lib.mkIf (tuneMem || tuneGpu) {
-      description = "Tunes nvidia gpu and memory clocks.";
+      description = "NVIDIA system tuning actions";
       wantedBy = ["multi-user.target"];
-      script = ''
-        ${tuneMemCmd}
-        ${tuneGpuCmd}
-      '';
+      script = let
+        smi = "/run/current-system/sw/bin/nvidia-smi";
+      in
+        [
+          {
+            cond = tuneMem;
+            value = "${smi} --lock-memory-clocks=0,${cfg.maxMemClock}";
+          }
+          {
+            cond = tuneGpu;
+            value = "${smi} --lock-gpu-clocks=0,${cfg.maxGpuClock}";
+          }
+        ]
+        |> lib.filter (v: v.cond)
+        |> map (v: v.value)
+        |> lib.concatStringsSep "\n";
     };
   };
 }
