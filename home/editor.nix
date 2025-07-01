b1c679920f7da16223881bc8c373c6ea0b69ab11
Joshua Park <git@joshprk.me>
2025-07-01 00:16:38 -0400

    feat: add temporary nvim config

--- home/editor.nix
+++ home/editor.nix
@@ -9,10 +9,19 @@
   ];
 
   dotfiles.".config/nvim/init.lua".text = ''
+    vim.o.cindent = true
     vim.o.expandtab = true
     vim.o.tabstop = 2
     vim.o.softtabstop = 2
     vim.o.shiftwidth = 2
+    vim.o.swapfile = false
+    vim.o.undofile = true
+
+    vim.o.cursorline = true
+    vim.o.number = true
+    vim.o.relativenumber = true
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   '';
 
   environment.sessionVariables = {
