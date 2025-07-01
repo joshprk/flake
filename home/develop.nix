c65f8f6eb9bd7afd9bc3d58f6373c829041b17e1
Joshua Park <git@joshprk.me>
2025-07-01 02:48:13 -0400

    feat: respect xdg for certain languages

--- home/develop.nix
+++ home/develop.nix
@@ -29,7 +29,24 @@
     };
   };
 
+  dotfiles.".config/npm/npmrc".text = ''
+    prefix=.config/npm
+    cache=.cache/npm
+    init-module=.config/npm/config/npm-init.js
+    logs-dir=.local/state/npm/logs
+  '';
+
   shell.init = ''
     ${lib.getExe pkgs.direnv} hook fish | source
   '';
+
+  environment.sessionVariables = {
+    CARGO_HOME = ".local/share/cargo";
+    GOPATH = ".local/share/go";
+    GOMODCACHE = ".cache/go/mod";
+    PYTHON_HISTORY = ".local/state/python_history";
+    NPM_CONFIG_USERCONFIG = ".config/npm/npmrc";
+    NODE_REPL_HISTORY = ".local/share/node_repl_history";
+    RUSTUP_HOME = ".local/share/rustup";
+  };
 }
