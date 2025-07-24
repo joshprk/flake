580d663acd11ec776f33fb0b4f7d4cda617cb74d
Joshua Park <git@joshprk.me>
2025-07-23 21:51:58 -0400

    hosts/forge: add metadata

--- hosts/alpine/default.nix
+++ hosts/alpine/default.nix
@@ -9,4 +9,13 @@
   features = {
     desktop = true;
   };
+
+  modules.system = {
+    nvidia.enable = true;
+    nvidia.maxMemClock = 10001;
+    nvidia.maxGpuClock = 1700;
+    nvidia.prime.offload.enable = true;
+    nvidia.prime.nvidiaBusId = "PCI:1:0:0";
+    nvidia.prime.amdgpuBusId = "PCI:13:0:0";
+  };
 }
