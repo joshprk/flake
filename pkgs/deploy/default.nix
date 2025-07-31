3157d42d5ee72e1e1863b940c27c2454eaf0424e
Joshua Park <git@joshprk.me>
2025-07-31 09:47:29 -0400

    pkgs/deploy: setup dependencies

--- pkgs/deploy/default.nix
+++ pkgs/deploy/default.nix
@@ -1,4 +1,13 @@
-{python3Packages, ...}:
+{
+  disko,
+  facter,
+  git,
+  git-credential-manager,
+  nix,
+  python3Packages,
+  sops,
+  ...
+}:
 python3Packages.buildPythonApplication {
   pname = "deploy";
   version = "0.1.0";
@@ -9,5 +18,14 @@ python3Packages.buildPythonApplication {
 
   nativeBuildInputs = with python3Packages; [
     setuptools
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    disko
+    facter
+    git
+    git-credential-manager
+    nix
+    sops
   ];
 }
--- pkgs/deploy/main.py
+++ pkgs/deploy/main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from deploy!")
+    pass
 
 if __name__ == "__main__":
     main()
