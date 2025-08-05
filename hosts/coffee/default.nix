91ba3016a7b833dc05dbef4f82e93ede341c09ac
Joshua Park <git@joshprk.me>
2025-08-04 22:50:04 -0400

    hosts/coffee: add nvidia configuration

--- hosts/coffee/default.nix
+++ hosts/coffee/default.nix
@@ -12,5 +12,10 @@
 
   modules.system = {
     nvidia.enable = true;
+    nvidia.maxMemClock = 12001;
+    nvidia.maxGpuClock = 1700;
+    nvidia.prime.offload.enable = true;
+    nvidia.prime.nvidiaBusId = "PCI:1:0:0";
+    nvidia.prime.amdgpuBusId = "PCI:53:0:0";
   };
 }
