a7aed9b56aa6ed5f0ef9da1fa304170275fc3a05
Joshua Park <git@joshprk.me>
2025-10-17 22:15:07 -0400

    home/hyprpaper: make into service module

--- home/default.nix
+++ home/default.nix
@@ -107,10 +107,6 @@
  
   services.hyprpaper = {
     enable = true;
-    wallpaper = pkgs.fetchurl {
-      url = "https://github.com/foxt/macOS-Wallpapers/blob/master/Mojave%20Night.jpg?raw=true";
-      hash = "sha256-Zv7uvjSNACpI2Yck22bsA8gwVaju2Yght7y09xko9xw=";
-    };
   };
 
   gtk = {
--- home/services/hyprpaper.nix
+++ home/services/hyprpaper.nix
@@ -12,11 +12,17 @@ in {
 
     wallpaper = lib.mkOption {
       type = lib.types.package;
+      default = pkgs.fetchurl {
+        url = "https://github.com/foxt/macOS-Wallpapers/blob/master/Mojave%20Night.jpg?raw=true";
+        hash = "sha256-Zv7uvjSNACpI2Yck22bsA8gwVaju2Yght7y09xko9xw=";
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    packages = [cfg.package];
+    packages = [
+      cfg.package
+    ];
 
     xdg.config.files."hypr/hyprpaper.conf".text = ''
       preload = ${cfg.wallpaper}
@@ -24,16 +30,19 @@ in {
     '';
 
     systemd.services.hyprpaper = {
+      description = "hyprpaper";
       wantedBy = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      partOf = ["graphical-session.target"];
+
+      restartTriggers = [
+        config.xdg.config.files."hypr/hyprpaper.conf".source
+      ];
+
       unitConfig = {
         ConditionEnvironment = "WAYLAND_DISPLAY";
-        Description = "hyprpaper";
-        After = ["graphical-session.target"];
-        PartOf = ["graphical-session.target"];
-        X-Restart-Triggers = [
-          "${config.xdg.config.files."hypr/hyprpaper.conf".source}"
-        ];
       };
+
       serviceConfig = {
         ExecStart = "${lib.getExe cfg.package}";
         Restart = "always";
