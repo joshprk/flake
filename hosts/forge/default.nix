878c1e41ca97d3024413ebb28aa61a3b7041b951
Joshua Park <git@joshprk.me>
2025-10-27 21:29:41 -0400

    hosts/forge: add ssh key

--- hosts/forge/default.nix
+++ hosts/forge/default.nix
@@ -6,5 +6,9 @@
   networking.hostName = "forge";
   system.stateVersion = "25.11";
 
+  users.users.root.openssh.authorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBfJqj0prGFq3v6KMo/Ll1sC6y57HZ2Bn/Bqi76srII+"
+  ];
+
   features = {};
 }
