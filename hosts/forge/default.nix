43bc3aabc21b4e6199de6fff8f154a60c2873377
Joshua Park <git@joshprk.me>
2025-10-28 22:17:54 -0400

    hosts/forge: enable ssh server

--- hosts/forge/default.nix
+++ hosts/forge/default.nix
@@ -6,9 +6,15 @@
   networking.hostName = "forge";
   system.stateVersion = "25.11";
 
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBfJqj0prGFq3v6KMo/Ll1sC6y57HZ2Bn/Bqi76srII+"
-  ];
+  services.openssh = {
+    enable = true;
+  };
+
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBfJqj0prGFq3v6KMo/Ll1sC6y57HZ2Bn/Bqi76srII+"
+    ];
+  };
 
   hjem.users.root = {};
 
