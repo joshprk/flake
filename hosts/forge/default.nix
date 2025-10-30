63110c5f25db6dbc8479324aae86b868249f4a7f
Joshua Park <git@joshprk.me>
2025-10-30 01:46:24 -0400

    hosts/forge: make forge user-friendly

--- hosts/forge/default.nix
+++ hosts/forge/default.nix
@@ -8,16 +8,16 @@
     ./disko.nix
   ];
 
+  features = {
+    users = true;
+  };
+
   networking.hostName = "forge";
   system.stateVersion = "25.11";
 
   modules.system = {
     network.exitNode = true;
     network.disableResolvedStub = true;
-  };
-
-  services.openssh = {
-    enable = true;
   };
 
   services.resolved = {
@@ -38,19 +38,10 @@
     };
   };
 
-  hjem = {
-    extraModules = lib.mkForce [];
-    linker = lib.mkForce null;
-  };
-
-  users.users.root = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGeVLsuetKBY7566XGnq3lmvVf9Lgs/ACu5Y1lXadaAw"
-    ];
-  };
-
   networking.firewall = {
     allowedTCPPorts = [53 80 443];
     allowedUDPPorts = [53];
   };
+
+  zramSwap.enable = true;
 }
