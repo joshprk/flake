ba6ca6bfc25d844325e6892dc464a3d23498b06e
Joshua Park <git@joshprk.me>
2025-10-30 01:41:39 -0400

    nixos/users: init

--- nixos/features/desktop.nix
+++ nixos/features/desktop.nix
@@ -11,50 +11,12 @@ in {
   };
 
   config = lib.mkIf cfg.desktop {
+    features = {
+      users = true;
+    };
+
     modules.apps = {
-      docker.enable = true;
-      flatpak.enable = true;
       niri.enable = true;
-    };
-
-    modules.system = {
-      kernel.package = pkgs.linuxPackages_zen;
-    };
-
-    environment.shellAliases = {
-      l = "ls -alh --group-directories-first";
-      ll = "ls -l --group-directories-first";
-      ls = "ls --color=tty --group-directories-first";
-    };
-
-    programs.fish = {
-      enable = true;
-    };
-
-    # TODO: Figure out a framework to handle per-user secrets
-    age.secrets.openrouter = {
-      owner = "josh";
-    };
-
-    hjem = {
-      users.josh = {
-        programs.fish.shellInit = ''
-          export OPENROUTER_API_KEY=$(cat ${config.age.secrets.openrouter.path})
-        '';
-      };
-    };
-
-    users = {
-      users.root = {
-        hashedPasswordFile = config.age.secrets.password.path;
-      };
-
-      users.josh = {
-        isNormalUser = true;
-        extraGroups = ["wheel"] ++ lib.optionals config.modules.apps.docker.enable ["docker"];
-        hashedPasswordFile = config.age.secrets.password.path;
-      };
-      defaultUserShell = pkgs.fish;
     };
 
     zramSwap.enable = true;
---
+++ nixos/features/users.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.features;
+in {
+  options.features = {
+    users = lib.mkEnableOption "the users feature";
+  };
+
+  config = lib.mkIf cfg.users {
+    modules.apps = {
+      docker.enable = true;
+      flatpak.enable = true;
+    };
+
+    modules.system = {
+      kernel.package = pkgs.linuxPackages_zen;
+    };
+
+    environment.shellAliases = {
+      l = "ls -alh --group-directories-first";
+      ll = "ls -l --group-directories-first";
+      ls = "ls --color=tty --group-directories-first";
+    };
+
+    programs.fish = {
+      enable = true;
+    };
+
+    # TODO: Figure out a framework to handle per-user secrets
+    age.secrets.openrouter = {
+      owner = "josh";
+    };
+
+    hjem = {
+      users.josh = {
+        programs.fish.shellInit = ''
+          export OPENROUTER_API_KEY=$(cat ${config.age.secrets.openrouter.path})
+        '';
+      };
+    };
+
+    users = {
+      users.root = {
+        hashedPasswordFile = config.age.secrets.password.path;
+      };
+
+      users.josh = {
+        isNormalUser = true;
+        extraGroups = ["wheel"] ++ lib.optionals config.modules.apps.docker.enable ["docker"];
+        hashedPasswordFile = config.age.secrets.password.path;
+      };
+      defaultUserShell = pkgs.fish;
+    };
+  };
+}
