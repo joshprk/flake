dbcd0530edf7d6c9bbfdca66730fb9540e3d51aa
Joshua Park <git@joshprk.me>
2025-12-07 20:00:33 -0500

    chore: remove unnecessary fix for faulty firmware update

--- hosts/alpine/default.nix
+++ hosts/alpine/default.nix
@@ -22,8 +22,4 @@
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c547", ATTR{power/wakeup}="disabled"
   '';
-
-  hardware.firmware = [
-    pkgs.linux-firmware
-  ];
 }
