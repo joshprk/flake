f208a5f62e2f87c0c852f453739cd4bbbe26d353
Joshua Park <git@joshprk.me>
2025-12-11 16:09:49 -0500

    fix(home): change hypridle sleep to work with hyprland

--- home/services/hypridle.nix
+++ home/services/hypridle.nix
@@ -18,7 +18,8 @@ in {
     xdg.config.files."hypr/hypridle.conf".text = ''
       listener {
         timeout = 900
-        on-timeout = ${lib.getExe pkgs.niri} msg action power-off-monitors
+        on-timeout = ${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms off
+        on-resume = ${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms on
       }
     '';
 
