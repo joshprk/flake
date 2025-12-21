c84c1604285684800862d4bb654ba58d0e185468
Joshua Park <git@joshprk.me>
2025-12-21 12:06:30 -0500

    chore: update lock file

--- flake.nix
+++ flake.nix
@@ -9,8 +9,7 @@
     };
 
   inputs = {
-    # nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-    nixpkgs.url = "github:joshprk/nixpkgs";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
 
     agenix = {
       url = "github:ryantm/agenix";
