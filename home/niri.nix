59c8f4f7a15bb70536cf78c9ec1eb8508603bc67
Joshua Park <git@joshprk.me>
2026-04-05 14:07:01 -0400

    feat: add ghostty shortcut in case of vicinae errors

--- home/niri.nix
+++ home/niri.nix
@@ -97,6 +97,7 @@
     binds {
       Mod+Shift+Slash { show-hotkey-overlay; }
       Mod+Tab { spawn "vicinae" "toggle"; }
+      Mod+T { spawn "ghostty"; }
       XF86AudioRaiseVolume allow-when-locked=true { spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+"; }
       XF86AudioLowerVolume allow-when-locked=true { spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-"; }
       XF86AudioMute allow-when-locked=true { spawn "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle"; }
