76ece1d5ac1fd5ae82afb03cf2ab50cf5ccf606e
Joshua Park <git@joshprk.me>
2026-04-05 04:09:19 -0400

    feat: lower pipewire quantum to reduce audio latency

--- nixos/apps/niri.nix
+++ nixos/apps/niri.nix
@@ -54,6 +54,34 @@ in {
       enable = true;
       pulse.enable = true;
       wireplumber.enable = true;
+
+      extraConfig.pipewire."92-quantum" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 1024;
+          "default.clock.min-quantum" = 1024;
+          "default.clock.max-quantum" = 1024;
+        };
+      };
+
+      extraConfig.pipewire-pulse."92-quantum" = {
+        context.modules = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              pulse.min.req = "1024/48000";
+              pulse.default.req = "1024/48000";
+              pulse.max.req = "1024/48000";
+              pulse.min.quantum = "1024/48000";
+              pulse.max.quantum = "1024/48000";
+            };
+          }
+        ];
+        stream.properties = {
+          node.latency = "32/48000";
+          resample.quality = 1;
+        };
+      };
     };
 
     services.power-profiles-daemon = {
