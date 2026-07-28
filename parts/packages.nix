ea00bb57e2c9cf5aa778872bead93ddcb79c8c7c
Joshua Park <git@joshprk.me>
2026-07-28 00:46:26 -0400

    refactor: remove deprecated old packages example comment

--- parts/packages.nix
+++ parts/packages.nix
@@ -5,21 +5,6 @@
     pkgs,
     ...
   }: {
-    ## Preserved to serve as an example in the future.
-    ## Remove when a package is added.
-    /*
-    packages.deploy = pkgs.callPackage ../pkgs/deploy {
-      disko = localInputs.disko.packages.${pkgs.system}.default;
-      facter = config.packages.facter;
-    };
-
-    packages.facter = pkgs.fetchFromGitHub {
-      owner = "nix-community";
-      repo = "nixos-facter";
-      rev = "main";
-      hash = "sha256-4kER7CyFvMKVpKxCYHuf9fkkYVzVK9AWpF55cBNzPc0=";
-    };
-    */
     packages.bootstrap = pkgs.writeShellScriptBin "flake-bootstrap" ''
       if [ ! -f /nix/var/agenix/host_key ]; then
         ${lib.getExe' pkgs.age "age-keygen"} -o /nix/var/agenix/host_key
