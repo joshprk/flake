fde4a8609939ba9f720d741fdabc832faf55ea1c
Joshua Park <git@joshprk.me>
2026-08-03 17:59:26 -0400

    refactor: move installation script and image config into module.nix

--- module.nix
+++ module.nix
@@ -20,12 +20,69 @@
     });
 
   perSystem = {pkgs, ...}: let
-    installEnv = lib.nixosSystem {
-      specialArgs.system = pkgs.system;
-      modules = [./image.nix];
-    };
+    mkSystem = cfg: lib.nixosSystem {modules = [cfg];};
+    mkImage = cfg: (mkSystem cfg).config.system.build.isoImage;
   in {
-    packages.image = installEnv.config.system.build.isoImage;
-    packages.wizard = pkgs.callPackage ./wizard {};
+    packages.image = mkImage ({modulesPath, ...}: {
+      imports = ["${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"];
+      networking.networkmanager.enable = true;
+      nixpkgs.hostPlatform = pkgs.stdenv.hostPlatform.system;
+      nix.settings.experimental-features = [
+        "flakes"
+        "nix-command"
+        "pipe-operators"
+      ];
+    });
+
+    packages.install = pkgs.writers.writeBashBin "flake-install" {} ''
+      set -euo pipefail
+
+      if [[ $EUID -ne 0 ]]; then
+        echo "install: this script must be run as root" >&2
+        exit 1
+      fi
+
+      if [[ $# -lt 1 ]]; then
+        echo "install: no hostname provided" >&2
+        exit 1
+      fi
+
+      flake="${./.}#$1"
+      password=$(${lib.getExe pkgs.openssl} rand -hex 32)
+
+      echo "install: running disko"
+
+      disko \
+        --mode destroy,format,mount \
+        --yes-wipe-all-disks \
+        --flake "$flake" \
+        < <(yes "$password")
+
+      for device in /dev/mapper/*; do
+        if cryptsetup status "$device" >/dev/null 2>&1; then
+          dm_name=$(basename "$(readlink -f "$device")")
+          luks_part="/dev/$(ls "/sys/class/block/$dm_name/slaves")"
+
+          echo "install: enrolling $luks_part to tpm"
+
+          if echo -n "$password" | systemd-cryptenroll \
+            --tpm2-device=auto \
+            --tpm2-pcrs=0,2,7,12 \
+            --unlock-key-file=/dev/stdin \
+            "$luks_part"; then
+            echo "install: successfully enrolled $luks_part"
+          else
+            echo "install: failed to enroll $luks_part" >&2
+          fi
+        fi
+      done
+
+      echo "install: running nixos-install"
+
+      unset password
+      nixos-install --no-root-password --flake "$flake"
+
+      echo "install: completed installation"
+    '';
   };
 }
