fe8636052d2fc9655df4affb3fda7b655b02ac10
Joshua Park <git@joshprk.me>
2026-08-04 03:18:22 -0400

    feat(home): add git

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,20 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./dconf.nix
   ];
+
+  packages = with pkgs; [
+    git
+  ];
+
+  xdg.config.files."git/config" = {
+    generator = (pkgs.formats.toml {}).generate "gitconfig";
+    value = {
+      init.defaultBranch = "main";
+      user.name = "Joshua Park";
+      user.email = "git@joshprk.me";
+    };
+  };
 
   xdg.config.files."Yubico/u2f_keys" = {
     source = ./u2f_keys;
