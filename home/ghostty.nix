abf55250bf0bf73c6347d364ffad099b29a1a860
Joshua Park <git@joshprk.me>
2026-08-12 14:21:12 -0400

    feat(home): modify ghostty config for ergonomics

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -9,9 +9,11 @@
       mkKeyValue = lib.generators.mkKeyValueDefault {} " = ";
     }).generate "ghostty-config";
     value = {
+      auto-update = "off";
       font-family = "IBM Plex Mono";
       font-size = 9;
       theme = "noctalia";
+      window-padding-color = "extend";
     };
   };
 }
