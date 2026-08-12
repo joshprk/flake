39f4742a546265361e81ab1cdaaac98906ca7b42
Joshua Park <git@joshprk.me>
2026-08-12 14:19:32 -0400

    feat(home): add minimal nvf config

--- home/nvf.nix
+++ home/nvf.nix
@@ -1,8 +1,43 @@
 {pkgs, ...}: let
   nvfWithConfig = pkgs.nvf {
-    vim.options.expandtab = true;
-    vim.options.shiftwidth = 2;
-    vim.options.tabstop = 2;
+    vim = {
+      autocomplete.blink-cmp.enable = true;
+      autopairs.nvim-autopairs.enable = true;
+      binds.whichKey = {
+        enable = true;
+        setupOpts.preset = "helix";
+      };
+      hideSearchHighlight = true;
+      languages = {
+        python.enable = true;
+        nix.enable = true;
+        markdown.enable = true;
+        rust.enable = true;
+        typescript.enable = true;
+        tsx.enable = true;
+      };
+      options = {
+        expandtab = true;
+        laststatus = 3;
+        scrolloff = 7;
+        shiftwidth = 2;
+        signcolumn = "no";
+        tabstop = 2;
+        wrap = false;
+      };
+      telescope.enable = true;
+      utility = {
+        direnv.enable = true;
+        motion.flash-nvim.enable = true;
+        oil-nvim.enable = true;
+      };
+      undoFile.enable = true;
+      visuals = {
+        highlight-undo.enable = true;
+        indent-blankline.enable = true;
+        nvim-web-devicons.enable = true;
+      };
+    };
   };
 in {
   packages = [nvfWithConfig];
