98d507fd7250f243b24a841af7f181c55f02e899
Joshua Park <git@joshprk.me>
2026-08-21 15:47:47 -0400

    feat(nixos): switch default session to hyprland
    Niri suffers from fence fallback timer expired issues on alpine

--- nixos/features/desktop.nix
+++ nixos/features/desktop.nix
@@ -54,7 +54,7 @@ in {
         cursorTheme.name = cursorTheme;
         settings = {
           auth.allow_empty_password = true;
-          session.default = "niri";
+          session.default = "hyprland";
         };
       };
       flatpak = {
